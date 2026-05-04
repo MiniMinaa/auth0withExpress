0055e9da84d2734b48009f3a68e0f46e67b122aa
Mina R <minarumang@icloud.com>
2026-05-04 21:48:44 +0200

    Add users email and fix img tag

--- backend/index.js
+++ backend/index.js
@@ -39,5 +39,5 @@ app.get("/profile", requiresAuth(), (req, res) => {
 //   });
 // });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
